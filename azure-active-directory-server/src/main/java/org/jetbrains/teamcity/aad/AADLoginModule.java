0c83f7f9e6f4f2aec880e3799050b667a6947841
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2014-11-16 14:14:37 +0300

    login module dummy implementation

--- azure-active-directory-server/src/main/java/org/jetbrains/teamcity/aad/AADLoginModule.java
+++ azure-active-directory-server/src/main/java/org/jetbrains/teamcity/aad/AADLoginModule.java
@@ -1,7 +1,13 @@
 package org.jetbrains.teamcity.aad;
 
+import jetbrains.buildServer.serverSide.auth.ServerPrincipal;
+import jetbrains.buildServer.serverSide.auth.TeamCityFailedLoginException;
+
 import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
 import javax.security.auth.login.LoginException;
 import java.util.Map;
 
@@ -9,23 +15,53 @@ import java.util.Map;
  * @author Evgeniy.Koshkin
  */
 public class AADLoginModule implements javax.security.auth.spi.LoginModule {
-  public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
 
+  private Subject mySubject;
+  private CallbackHandler myCallbackHandler;
+  private Callback[] myCallbacks;
+  private NameCallback myNameCallback;
+  private PasswordCallback myPasswordCallback;
+
+  public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
+    myCallbackHandler = callbackHandler;
+    myNameCallback = new NameCallback("login:");
+    myPasswordCallback = new PasswordCallback("password:", false);
+    myCallbacks = new Callback[]{myNameCallback, myPasswordCallback};
+    mySubject = subject;
   }
 
   public boolean login() throws LoginException {
-    return false;
+    try {
+      myCallbackHandler.handle(myCallbacks);
+    }
+    catch (Throwable t) {
+      throw new jetbrains.buildServer.serverSide.auth.TeamCityLoginException(t);
+    }
+
+    final String login = myNameCallback.getName();
+    final String password = new String(myPasswordCallback.getPassword());
+
+    if (checkPassword(login, password)) {
+      mySubject.getPrincipals().add(new ServerPrincipal(null, login));
+      return true;
+    }
+
+    throw new TeamCityFailedLoginException();
   }
 
   public boolean commit() throws LoginException {
-    return false;
+    return true;
   }
 
   public boolean abort() throws LoginException {
-    return false;
+    return true;
   }
 
   public boolean logout() throws LoginException {
-    return false;
+    return true;
+  }
+
+  private boolean checkPassword(String login, String password) {
+    return true;
   }
 }
