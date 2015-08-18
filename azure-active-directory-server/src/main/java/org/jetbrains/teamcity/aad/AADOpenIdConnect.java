445842a10feec14a520b017ccc13e392519effad
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2015-08-18 21:17:03 +0300

    fixed #10 - formed valid url based on changed app auth 2.0 endpoint

--- azure-active-directory-server/src/main/java/org/jetbrains/teamcity/aad/AADOpenIdConnect.java
+++ azure-active-directory-server/src/main/java/org/jetbrains/teamcity/aad/AADOpenIdConnect.java
@@ -8,6 +8,6 @@ import org.jetbrains.annotations.NotNull;
 public class AADOpenIdConnect {
   @NotNull
   public static String getRequestUrl(@NotNull final String appEndpoint, @NotNull final String clientId, @NotNull final String nonce) {
-    return String.format("%s&response_type=id_token&client_id=%s&scope=openid&nonce=%s&response_mode=form_post", appEndpoint, clientId, nonce);
+    return String.format("%s?response_type=id_token&client_id=%s&scope=openid&nonce=%s&response_mode=form_post", appEndpoint, clientId, nonce);
   }
 }
