f659f6a25117ac21e3d76ff3ea508ff46e5cd338
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2015-08-19 14:40:12 +0300

    fixed #10 - handled legacy app endpoint format

--- azure-active-directory-server/src/main/java/org/jetbrains/teamcity/aad/AADOpenIdConnect.java
+++ azure-active-directory-server/src/main/java/org/jetbrains/teamcity/aad/AADOpenIdConnect.java
@@ -8,6 +8,8 @@ import org.jetbrains.annotations.NotNull;
 public class AADOpenIdConnect {
   @NotNull
   public static String getRequestUrl(@NotNull final String appEndpoint, @NotNull final String clientId, @NotNull final String nonce) {
-    return String.format("%s?response_type=id_token&client_id=%s&scope=openid&nonce=%s&response_mode=form_post", appEndpoint, clientId, nonce);
+    return appEndpoint.contains("?")
+            ? String.format("%s&response_type=id_token&client_id=%s&scope=openid&nonce=%s&response_mode=form_post", appEndpoint, clientId, nonce) //handle legacy appEndpoint format
+            : String.format("%s?response_type=id_token&client_id=%s&scope=openid&nonce=%s&response_mode=form_post", appEndpoint, clientId, nonce);
   }
 }
