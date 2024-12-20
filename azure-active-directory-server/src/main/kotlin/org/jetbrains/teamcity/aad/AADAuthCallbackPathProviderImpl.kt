cf4d5ebf8b0f894e2db0d315b85705e4412d2aa3
Ilya Voronin <voroninilya115@gmail.com>
2024-12-20 20:08:00 +0400

    TW-90029 user is redirected to the new ui(or the page he wanted to visit) by default, no need to specify teamcity.aad.endpoint.type=dedicated internal property updated readme

--- azure-active-directory-server/src/main/kotlin/org/jetbrains/teamcity/aad/AADAuthCallbackPathProviderImpl.kt
+++ azure-active-directory-server/src/main/kotlin/org/jetbrains/teamcity/aad/AADAuthCallbackPathProviderImpl.kt
@@ -6,7 +6,7 @@ import jetbrains.buildServer.serverSide.TeamCityProperties
 
 class AADAuthCallbackPathProviderImpl : AADAuthCallbackPathProvider {
     override val path: String
-        get() = when(TeamCityProperties.getPropertyOrNull(AADConstants.ENDPOINT_TYPE_PROPERTY)?.toLowerCase()) {
+        get() = when(TeamCityProperties.getProperty(AADConstants.ENDPOINT_TYPE_PROPERTY, AADConstants.DEDICATED_ENDPOINT_TYPE).toLowerCase()) {
             AADConstants.DEDICATED_ENDPOINT_TYPE -> AADConstants.DEDICATED_AUTH_PATH
             else -> AADConstants.OVERVIEW_PATH
         }
